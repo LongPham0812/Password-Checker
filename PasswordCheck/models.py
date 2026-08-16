05028b943c3858a788148fa20753f6c44d2014b6
Long Pham <longpham@sandiego.edu>
2026-08-15 23:32:42 -0700

    Added admin stuff

--- PasswordCheck/models.py
+++ PasswordCheck/models.py
@@ -6,5 +6,14 @@ class Password(models.Model):
     password_strength = models.IntegerField(default=0)
     password_attributes = models.JSONField(default=dict)
 
+    def get_password_text(self):
+        return self.password_text
+
+    def get_password_strength(self):
+        return self.password_strength
+
+    def get_password_attributes(self):
+        return self.password_attributes
+
     def __str__(self):
         return f"{self.password_text} {self.password_strength} {self.password_attributes}"
