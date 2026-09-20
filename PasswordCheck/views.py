905df0b44787e2c75391b7da690a05d4cf8a5686
Long Pham <longpham@sandiego.edu>
2026-09-20 12:47:47 -0700

    Added views

--- PasswordCheck/views.py
+++ PasswordCheck/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Password Checker")
+    return HttpResponse("Password Checker - Enter a password here.")
+
+def evaluate_password(request, password, strength):
+    return HttpResponse("Password \"%s\" has strength score of %s" % (password, strength))
